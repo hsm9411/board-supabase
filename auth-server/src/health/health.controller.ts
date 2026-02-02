0c15a9245e59564adcf46e830007c16437422dc8
hsm <hsm@hsm>
2026-02-02 18:29:45 +0900

    Fix: nested git issue

---
+++ auth-server/src/health/health.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+
+@ApiTags('health')
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  @ApiOperation({ summary: '서비스 헬스체크' })
+  check() {
+    return this.health.check([
+      () => this.db.pingCheck('database'),
+    ]);
+  }
+}
